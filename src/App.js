d1cdcd81047efb73aa5ba4d68364589955c90d41
Bickor <martin.heberling.dalies@gmail.com>
2024-05-13 20:04:38 -0700

    Trying to add the infowindow to the marker.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,8 @@ function App() {
     <div className="App" style={{ height: '100vh', width: '20%', float: "left" }}>
       <h1>Stickermap</h1>
       <p>Stickers!!</p>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" style={{ float: "left" }}></input>
+      <div style={{ float: "left" }}>Add Marker</div>
     </div>
   );
 }
--- src/mapComponent.js
+++ src/mapComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { APIProvider, Map, InfoWindow, Marker } from '@vis.gl/react-google-maps';
+import { MarkerWithInfowindow } from './markerWithInfoWindow';
 import data from "./secrets.json"
 
 const API_KEY = data["key"];
@@ -15,14 +16,16 @@ export default class SimpleMap extends React.Component {
 
   addMarker(e) {
     console.log(e)
-    const newPlace = {
-      id: this.state.places.length,
-      lat: e.detail.latLng.lat,
-      lng: e.detail.latLng.lng,
-    };
-    this.setState({
-      places: [...this.state.places, newPlace]
-    })
+    if (document.getElementById('vehicle1').checked) {
+      const newPlace = {
+        id: this.state.places.length,
+        lat: e.detail.latLng.lat,
+        lng: e.detail.latLng.lng,
+      };
+      this.setState({
+        places: [...this.state.places, newPlace]
+      })
+    }
   }
 
   render() {
@@ -34,48 +37,33 @@ export default class SimpleMap extends React.Component {
             defaultCenter={{lat: 22.54992, lng: 0}}
             gestureHandling={'greedy'}
             disableDefaultUI={true}
-            // onClick={() => alert('Map was clicked!')}
             onClick={(e) => this.addMarker(e)}
           />
           {this.state.places.map(place => {
             return (
-              <Marker
-                key={place.id}
-                position={{ lat: place.lat, lng: place.lng}}
-              />
+              <div>
+                <Marker
+                  key={place.id}
+                  position={{ lat: place.lat, lng: place.lng}}
+                />
+                <InfoWindow position={{lat: place.lat, lng: place.lng}} maxWidth={200}>
+                  <p>
+                    This is the content for another infowindow with <em>HTML</em>
+                    -elements.
+                  </p>
+                </InfoWindow>
+                <MarkerWithInfowindow/>
+              </div>
             )
           })}
-            <Marker
+            {/* <Marker
               position={{lat: 10, lng: 10}}
               clickable={true}
               onClick={() => alert('marker was clicked!')}
               title={'clickable google.maps.Marker'}
-            />
+            /> */}
         </APIProvider>
       </div>
     )
   }
 }
-
-// const SimpleMap = () => {
-//   return (
-//     <div style={{ height: '100vh', width: '80%', float: "left" }}>
-//       <APIProvider apiKey={API_KEY}>
-//         <Map
-//           defaultZoom={3}
-//           defaultCenter={{lat: 22.54992, lng: 0}}
-//           gestureHandling={'greedy'}
-//           disableDefaultUI={true}
-//         />
-//           <Marker
-//             position={{lat: 10, lng: 10}}
-//             clickable={true}
-//             onClick={() => alert('marker was clicked!')}
-//             title={'clickable google.maps.Marker'}
-//           />
-//       </APIProvider>
-//     </div>
-//   );
-// };
-
-// export default SimpleMap;
