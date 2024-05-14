cd1da8732c9c30b6f835eb489775dfd8975e0040
Martin Heberling <martin.heberling.dalies@gmail.com>
2024-05-13 23:34:54 -0700

    Sending data to firestore.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div className="App" style={{ height: '100vh', width: '20%', float: "left" }}>
       <h1>Stickermap</h1>
       <p>Stickers!!</p>
-      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" style={{ float: "left" }}></input>
-      <div style={{ float: "left" }}>Add Marker</div>
+      <div style={{ float: "center" }}>
+        <div>
+          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" style={{ float: "left" }}></input>
+          <div style={{ float: "left" }}>Add Marker</div>
+        </div>
+        <div>
+          <input type="checkbox" id="vehicle2" name="vehicle2" value="Bike" style={{ float: "left" }}></input>
+          <div style={{ float: "left" }}>Remove Marker</div>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/mapComponent.js
+++ src/mapComponent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { APIProvider, Map, InfoWindow, Marker } from '@vis.gl/react-google-maps';
 import { MarkerWithInfowindow } from './markerWithInfoWindow';
+import { collection, addDoc, GeoPoint } from "firebase/firestore";
+import {db} from './config.js';
 import data from "./secrets.json"
 
 const API_KEY = data["key"];
@@ -14,17 +16,44 @@ export default class SimpleMap extends React.Component {
     places: []
   }
 
-  addMarker(e) {
-    console.log(e)
+  async addMarker(e) {
     if (document.getElementById('vehicle1').checked) {
       const newPlace = {
         id: this.state.places.length,
         lat: e.detail.latLng.lat,
         lng: e.detail.latLng.lng,
       };
+
+      const newDoc = {
+        id: this.state.places.length,
+        geoPoint: new GeoPoint(e.detail.latLng.lat, e.detail.latLng.lng),
+      }
+      // const addMarkerToFirestore = async (e) => {
+        // e.preventDefault();  
+       
+        try {
+          const docRef = await addDoc(collection(db, "markers"), {
+            marker: newDoc,    
+          });
+          console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+    // }
+
       this.setState({
         places: [...this.state.places, newPlace]
       })
+    }
+  }
+
+  removeMarker(e) {
+    let lat = e.latLng.lat()
+    let lng = e.latLng.lng()
+    if (document.getElementById('vehicle2').checked) {
+      this.setState(prevState => ({
+        places: prevState.places.filter(place => !(place.lat === lat && place.lng === lng))
+      }));
     }
   }
 
@@ -33,6 +62,7 @@ export default class SimpleMap extends React.Component {
       <div style={{ height: '100vh', width: '80%', float: "left" }}>
         <APIProvider apiKey={API_KEY}>
           <Map
+            key={"Map"}
             defaultZoom={3}
             defaultCenter={{lat: 22.54992, lng: 0}}
             gestureHandling={'greedy'}
@@ -45,21 +75,23 @@ export default class SimpleMap extends React.Component {
                 <Marker
                   key={place.id}
                   position={{ lat: place.lat, lng: place.lng}}
+                  onClick={(e) => this.removeMarker(e)}
                 />
-                <InfoWindow position={{lat: place.lat, lng: place.lng}} maxWidth={200}>
+                {/* <InfoWindow position={{lat: place.lat, lng: place.lng}} maxWidth={200}>
                   <p>
                     This is the content for another infowindow with <em>HTML</em>
                     -elements.
                   </p>
-                </InfoWindow>
-                <MarkerWithInfowindow/>
+                </InfoWindow> */}
+                {/* <MarkerWithInfowindow/> */}
               </div>
             )
           })}
             {/* <Marker
               position={{lat: 10, lng: 10}}
               clickable={true}
-              onClick={() => alert('marker was clicked!')}
+              // onClick={() => alert('marker was clicked!')}
+              onClick={(e) => this.removeMarker(e)}
               title={'clickable google.maps.Marker'}
             /> */}
         </APIProvider>
